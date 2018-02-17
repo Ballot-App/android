856dc812db8b1a7fa1df34e7853ca624e9a1b82f
Tizzy M <tizzymac@icloud.com>
2018-02-17 13:27:28 +0000

    Initial setup

---
+++ app/src/main/java/com/biz/tizzy/eballot/StartFragment.java
@@ -0,0 +1,12 @@
+package com.biz.tizzy.eballot;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tizzy on 2/17/18.
+ */
+
+public class StartMenuFragment extends Fragment {
+
+    private static final String TAG = "StartFragment";
+}
