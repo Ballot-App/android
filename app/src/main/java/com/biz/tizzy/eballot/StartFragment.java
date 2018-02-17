ba9659c870d46f05a1004af66902cd77c221c7b8
Tizzy M <tizzymac@icloud.com>
2018-02-17 15:10:08 +0000

    Added Buttons to No Abstain

--- app/src/main/java/com/biz/tizzy/eballot/StartFragment.java
+++ app/src/main/java/com/biz/tizzy/eballot/StartFragment.java
@@ -1,10 +1,12 @@
 package com.biz.tizzy.eballot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -16,11 +18,26 @@ public class StartFragment extends Fragment {
     private static final String TAG = "StartFragment";
 
     private EditText mEnterCode;
+    private Button mButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_start, container, false);
 
+        mButton = (Button) view.findViewById(R.id.enterButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TEMP go to noAb
+                goToNoAbstain();
+            }
+        });
+
         return view;
+    }
+
+    private void goToNoAbstain() {
+        Intent intent = NoAbstainActivity.newIntent(getActivity());
+        startActivity(intent);
     }
 }
